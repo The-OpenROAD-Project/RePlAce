128baf3946cf72b19f83184aad334542eb2449e0
mgwoo <mgwoo@unist.ac.kr>
2019-05-03 17:01:22 -0700

    Add Simple Testcase

--- test/run.sh
+++ test/run.sh
@@ -5,15 +5,17 @@ get_abs_filename() {
   echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
-DETAIL_PLACER_FLAG="NTU3"
+DETAIL_PLACER_FLAG="NTU4"
 LEF="a2a_wb_dma_top/contest.lef"
 DEF="a2a_wb_dma_top/wb_dma_top_soce.def"
+LIB="a2a_wb_dma_top/contest.lib"
+SDC="a2a_wb_dma_top/wb_dma_top.sdc"
 VERILOG="a2a_wb_dma_top/wb_dma_top_soce.v"
 OUT="a2a_wb_dma_top_out"
-DETAIL_PLACE_LOC=$(get_abs_filename "/usr/local/bin/ntuplace3")
+DETAIL_PLACE_LOC=$(get_abs_filename "../ntuplace4")
 
 ../src/RePlAce -bmflag etc \
-  -lef $LEF -def $DEF -verilog $VERILOG \
+  -lef $LEF -def $DEF -verilog $VERILOG -lib $LIB -sdc $SDC \
   -output $OUT \
   -dpflag $DETAIL_PLACER_FLAG -dploc $DETAIL_PLACE_LOC \
-  -skipIP -bin 64 -plot
+  -skipIP -bin 64 -plot -timing -capPerMicron 0.23e-15 -resPerMicron 23
