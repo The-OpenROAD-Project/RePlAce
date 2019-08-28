880da4bf233b954bb94ffa620a6a3ba0897a61cb
mgwoo <mgwoo@unist.ac.kr>
2019-08-28 14:09:58 -0700

    Update test scripts for gcd TD/nonTD

--- test/gcd_td_test.tcl
+++ test/gcd_td_test.tcl
@@ -1,5 +1,5 @@
 # 
-# Examples for Non Timing-driven RePlAce with TCL usage
+# Examples for Timing-driven RePlAce with TCL usage
 #
 proc print_instances {rep} {
   set insts_cnt [rep get_instance_list_size]
@@ -13,31 +13,41 @@ set design gcd
 set lib_dir ./library/nangate45/
 set design_dir ./design/nangate45/${design}
 
-
 replace_external rep
 
 # Import LEF/DEF files
 rep import_lef ${lib_dir}/NangateOpenCellLibrary.lef
 rep import_def ${design_dir}/${design}.def
+
+# timing-driven parameters
+rep import_verilog ${design_dir}/${design}.v
+rep import_sdc ${design_dir}/${design}.sdc
+rep import_lib ${lib_dir}/NangateOpenCellLibrary_typical.lib
+
+rep set_unit_res 16
+rep set_unit_cap 0.23e-15
+rep set_timing_driven 1 
+
+# set output folder location
 rep set_output ./output/
 
 # Initialize RePlAce
 rep init_replace
 
-# place_cell with BiCGSTAB 
+# place_cell with initial place (BiCGSTAB)
 rep place_cell_init_place
 
+puts "initPlace HPWL: [rep get_hpwl]"
 
 # print out instances' x/y coordinates
 print_instances rep
 
-# place_cell with Nesterov method 
+# place_cell with Nesterov (RePlAce)
 rep place_cell_nesterov_place
 
 # print out instances' x/y coordinates
 print_instances rep
 
 # Export DEF file
-rep export_def ./gcd_nonTD.def
-puts "Final HPWL: [rep get_hpwl]"
-
+rep export_def ./gcd_TD.def
+puts "nesterovPlace HPWL: [rep get_hpwl]"
