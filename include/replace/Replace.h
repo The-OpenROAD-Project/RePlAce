3f5ae7afcf5c78fc94629be47a547622f1525935
mgwoo <mwoo@eng.ucsd.edu>
2020-01-09 09:54:35 -0700

    Update

--- include/replace/Replace.h
+++ include/replace/Replace.h
@@ -11,22 +11,20 @@ namespace sta {
 
 namespace GlobalPlacer {
 
-enum InitPlaceMode
-{
-  BiCGSTAB
-}
-
-
 class Replace
 {
   public:
     void setDb(odb::dbDatabase* odb);
     void setSta(sta::dbSta* dbSta);
 
-    void initPlace(InitPlaceMode initPlace);
+    void doInitPlace();
+    void doNesterovPlace();
+
     void setBinCntX(int binCntX);
     void setBinCntY(int binCntY);
 
+    void setTargetDensity(float density);
+    void setTargetOverflow(float overflow);
     void setInitLambda(float lambda);
     void setMinPCoef(float minPCoef);
     void setMaxPCoef(float maxPCoef);
@@ -35,6 +33,17 @@ class Replace
   private:
     odb::dbDatabase* odb_;
     sta::dbSta* sta_;
+
+    int maxInitPlaceIter_;
+    int maxNesterovPlaceIter_;
+    int binCntX_;
+    int binCntY_;
+    float overflow_;
+    float density_;
+    float lambda_;
+    float minPCoef_;
+    float maxPCoef_;
+    float deltaHpwl_;
 };
 
 }
