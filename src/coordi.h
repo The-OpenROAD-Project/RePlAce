379dfff3b2592ec84616181279a32f7ec1c1f920
mgwoo <mwoo@eng.ucsd.edu>
2020-01-27 11:00:32 -0800

    add coordi*

---
+++ src/coordi.h
@@ -0,0 +1,23 @@
+#ifndef __REPLACE_COORDI__
+#define __REPLACE_COORDI__
+
+namespace replace {
+
+class FloatCoordi {
+  public:
+  float x;
+  float y;
+  FloatCoordi();
+  FloatCoordi(float x, float y);
+};
+
+class IntCoordi {
+  public:
+  int x;
+  int y;
+  IntCoordi();
+  IntCoordi(int x, int y);
+};
+}
+
+#endif
