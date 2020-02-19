de81b20121a6d5dbc81adbffd2c59d3ff8c60db1
mgwoo <mwoo@eng.ucsd.edu>
2020-02-19 13:45:04 -0800

    testcase update for middle/large

--- test/large01.tcl
+++ test/large01.tcl
@@ -3,8 +3,9 @@ set test_name large01
 read_lef ./nangate45.lef
 read_def ./$test_name.def
 
-global_placement -verbose 5 -max_phi_coef 1.04 
+global_placement 
 set def_file [make_result_file $test_name.def]
 write_def $def_file
 diff_file $def_file $test_name.defok
+source report_hpwl.tcl
 exit
