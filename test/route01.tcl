d387c6db32a803fa030cbdff770c80fea38cda1a
mgwoo <mwoo@eng.ucsd.edu>
2020-03-03 17:31:29 -0800

    route testcase modify to use multiple global_place

--- test/route01.tcl
+++ test/route01.tcl
@@ -3,8 +3,11 @@ set test_name medium01
 read_lef ./nangate45.lef
 read_def ./$test_name.def
 
-global_placement
-# fastroute -write_route -write_est
+global_placement -disable_routability_driven
+fastroute -write_route -write_est
+global_placement 
+
+
 set def_file [make_result_file route01.def]
 write_def $def_file
 diff_file $def_file route01.defok
